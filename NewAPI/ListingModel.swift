1f4ca2a9f56655e9d4453bcf9a785cf100f2ced5
Alex Beattie <artisanbranding@me.com>
2023-10-03 17:46:25 -0700

    refactored projecct to use local script for testing

---
+++ NewAPI/ListingModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListingModel.swift
+//  NewAPI
+//
+//  Created by Alex Beattie on 10/3/23.
+//
+
+import Foundation
---
+++ NewAPI/ListingPublisherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListingPublisherViewModel.swift
+//  NewAPI
+//
+//  Created by Alex Beattie on 10/3/23.
+//
+
+import Foundation
